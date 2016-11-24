e73e75f1d5e0c59339f21e95d62b43d2634c444b
gm925 <gm925925@live.com>
2016-11-24 23:24:33 +0800

    add react

---
+++ app/static/js/index.js
@@ -0,0 +1,6 @@
+var ReactDOM = require('react-dom');
+var React = require('react');
+var Finance = require("./components/Finance");
+
+ReactDOM.render(<Finance/>,document.getElementById("finance-container"));
+
